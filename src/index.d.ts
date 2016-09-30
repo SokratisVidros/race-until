c0681f2bac821a022fe8ad278cb89daf6f7db9b9
Sokratis Vidros <sokratis.vidros@gmail.com>
2016-09-30 14:57:53 +0300

    Add typescript definitions.

---
+++ src/index.d.ts
@@ -0,0 +1,1 @@
+export { default as raceUntil, timebomb } from './raceUntil';
---
+++ src/raceUntil.d.ts
@@ -0,0 +1,2 @@
+export declare function timebomb(timeout?: number, softReject?: boolean): any;
+export default function raceUntil(p: any, timeout?: number, softReject?: boolean): any;
