e6e1ac39075d9fdf459ab5e6bdea15ca8abf3472
Sokratis Vidros <sokratis.vidros@gmail.com>
2019-07-23 22:38:07 +0300

    Upgrade test and building tooling

--- tools/build.js
+++ tools/build.js
@@ -3,7 +3,6 @@
 const fs = require('fs');
 const del = require('del');
 const rollup = require('rollup');
-const babel = require('rollup-plugin-babel');
 const pkg = require('../package.json');
 
 let promise = Promise.resolve();
@@ -12,21 +11,14 @@ let promise = Promise.resolve();
 promise = promise.then(() => del(['dist/*']));
 
 // Compile source code into a distributable format with Babel
-for (const format of ['es6', 'cjs', 'umd']) {
+for (const format of ['esm', 'cjs', 'umd']) {
   promise = promise.then(() => rollup.rollup({
-    entry: 'src/index.js',
-    external: Object.keys(pkg.dependencies),
-    plugins: [babel(Object.assign(pkg.babel, {
-      babelrc: false,
-      exclude: 'node_modules/**',
-      runtimeHelpers: true,
-      presets: pkg.babel.presets.map(x => (x === 'es2015' ? ['es2015', { modules: false }] : x)),
-    }))],
+    input: 'src/index.js'
   }).then(bundle => bundle.write({
-    dest: `dist/${format === 'cjs' ? 'index' : `index.${format}`}.js`,
+    file: `dist/${format === 'cjs' ? 'index' : `index.${format}`}.js`,
     format,
     sourceMap: true,
-    moduleName: format === 'umd' ? pkg.name : undefined,
+    name: format === 'umd' ? pkg.name : undefined,
   })));
 }
 
@@ -35,7 +27,7 @@ promise = promise.then(() => {
   delete pkg.devDependencies;
   delete pkg.scripts;
   delete pkg.eslintConfig;
-  delete pkg.babel;
+  // delete pkg.babel;
   fs.writeFileSync('dist/package.json', JSON.stringify(pkg, null, '  '), 'utf-8');
   fs.writeFileSync('dist/LICENSE.txt', fs.readFileSync('LICENSE.txt', 'utf-8'), 'utf-8');
 });
