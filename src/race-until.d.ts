1b0414348c4d8896df13390bb29db5707b341385
Nikos Kostoulas <nikostoulas@users.noreply.github.com>
2019-07-24 13:20:55 +0300

    Update race-until.d.ts

--- src/race-until.d.ts
+++ src/race-until.d.ts
@@ -1,2 +1,2 @@
-export function timebomb(timeout?: number, softReject?: boolean): any;
-export function raceUntil(p: any, timeout?: number, softReject?: boolean): any;
+export function timebomb(timeout?: number, timeoutResponse?: any): any;
+export function raceUntil(p: any, timeout?: number, timeoutResponse?: any): any;
